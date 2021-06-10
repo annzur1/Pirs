7e7c88956f301e8b446b59a87caecf106e631ef0
KubaStry <83343519+KubaStry@users.noreply.github.com>
2021-06-10 12:14:21 +0200

    Update cityofcolors_test.py
    Dodałem menu i zagęściłem siatkę

--- cityofcolors_test.py
+++ cityofcolors_test.py
@@ -1,24 +1,39 @@
 import pygame, sys, random
+from pygame import surface
+from pygame import image
 from pygame.math import Vector2
 
 pygame.init()
 
-
 #### KLASA GRACZA ####
 class Player:
     def __init__ (self):
-        self.x = 5.5
-        self.y= 5.5
-        self.pos = Vector2(self.x * cell_size, self.y * cell_size)
-        self.speed = 0.1
-        self.direction = Vector2(0,0)
+        self.pos = Vector2(0.5,0.5)
+        self.speed = 2
+        self.direction = Vector2(1,0)
+        self.stored_direction=None
         self.color = (pygame.Color("white"))
+        self.pix_pos = self.get_pix_pos()
+        self.can_move = True
     def update(self):
-        self.pos = self.pos + (self.direction * self.speed)
+        if self.can_move:
+            self.pix_pos += self.direction * self.speed
+        if self.time_of_move():
+            if self.stored_direction != None:
+                self.direction = self.stored_direction
     def draw_player(self):
-        pygame.draw.circle(screen, (self.color), (int(self.pos.x), int(self.pos.y)), cell_size//2)
+        pygame.draw.circle(screen, (self.color), (int(self.pix_pos.x), int(self.pix_pos.y)), cell_size//3)
     def move(self, direction):
-        self.direction = direction
+        self.stored_direction = direction
+    def get_pix_pos(self):
+        return Vector2((self.pos.x * cell_size)//2 + cell_number//2, (self.pos.y * cell_number)//2 + cell_number//2)
+    def time_of_move(self):
+        if int(self.pix_pos.x//2) % cell_size == 0:
+            if self.direction == Vector2(1, 0) or self.direction ==  Vector2(-1, 0) or self.direction ==  Vector2(0, 0):
+                return True
+        if int(self.pix_pos.y//2) % cell_number == 0:
+            if self.direction ==  Vector2(0, 1) or self.direction ==  Vector2(0, -1) or self.direction == Vector2(0, 0):
+                return True
 
 #### KLASA PRZECIWNIKA ####
 class Enemy:
@@ -63,7 +78,7 @@ class Stranger:
     def move(self, direction):
         self.direction = direction
     def randmove(self, movecounter):
-        
+
         if (movecounter % 120 == 0):
             self.randomizer = (random.randint(1, 4))
         if self.randomizer == 1:
@@ -74,19 +89,25 @@ class Stranger:
             self.move(Vector2(0, -1 * cell_size))
         else:
             self.move(Vector2(0, 1 * cell_size))
-        
 
 
-    
 
+### ZDEFINIOWANIE ISTOTNYCH WARTOŚCI ###
 
-cell_size = 12
-cell_number = 50
-screen = pygame.display.set_mode((cell_number*cell_size, cell_number*cell_size))
+width = 616
+height = 660
+cell_size = width//28
+cell_number = height//30
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-
-
-
+walls = []
+run = True
+menu = True
+credits = False
+menuimage = pygame.image.load('C:/Users/robal/Desktop/programowanie/img/menu2.jpg')
+creditsimage = pygame.image.load('C:/Users/robal/Desktop/programowanie/img/credits.jpg')
+imagerect = menuimage.get_rect()
+### TWORZENIE BYTÓW ###
 
 player = Player()
 enemy1 = Enemy()
@@ -96,52 +117,83 @@ stranger = Stranger()
 movecounter = 0
 
 
+### GŁÓWNA CZĘŚĆ ###
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:     ####poruszanie graczem
-            if event.key == pygame.K_LEFT:
-                player.move(Vector2(-1 * cell_size, 0))
-            if event.key == pygame.K_RIGHT:
-                player.move(Vector2(1 * cell_size, 0))
-            if event.key == pygame.K_UP:
-                player.move(Vector2(0, -1 * cell_size))
-            if event.key == pygame.K_DOWN:
-                player.move(Vector2(0, 1 * cell_size))
-            if event.key == pygame.K_1:
-                player.move(Vector2(0, 0))
-    
-    enemy1.randmove(movecounter) ####ruch bytów
-    enemy2.randmove(movecounter)
-    enemy3.randmove(movecounter)
-    stranger.randmove(movecounter)
-    movecounter += 1
-    
-    screen.fill((0,0,0))
-    player.draw_player()
-    player.update()
-    enemy1.draw_enemy()
-    enemy1.update()
-    enemy2.draw_enemy()
-    enemy2.update()
-    enemy3.draw_enemy()
-    enemy3.update()
-    stranger.draw_stranger()
-    stranger.update()
-    
-    #### INTERAKCJE POMIĘDZY GRACZEM A BYTAMI (testowo)####
-    if ((abs(player.pos.x - enemy1.pos.x) < cell_size) and (abs(player.pos.y - enemy1.pos.y) < cell_size)):
-        enemy1.color = (pygame.Color("blue"))
-    if ((abs(player.pos.x - enemy2.pos.x) < cell_size) and (abs(player.pos.y - enemy2.pos.y) < cell_size)):
-        enemy2.color = (pygame.Color("blue"))
-    if ((abs(player.pos.x - enemy1.pos.x) < cell_size) and (abs(player.pos.y - enemy3.pos.y) < cell_size)):
-        enemy3.color = (pygame.Color("blue"))
-    if ((abs(player.pos.x - stranger.pos.x) < cell_size) and (abs(player.pos.y - stranger.pos.y) < cell_size)):
-        player.color = (pygame.Color("yellow"))
-    
+while run:
+    if menu == True:
+        if credits == True:
+            screen.fill("white")
+            screen.blit(creditsimage, imagerect)
+        else:
+            screen.fill("white")
+            screen.blit(menuimage, imagerect)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_2:
+                        menu = False
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_3:
+                        pygame.quit()
+                        sys.exit()
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_4:
+                        if credits == False:
+                            credits = True
+                        else:
+                            credits = False
+    else:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:     ####poruszanie graczem
+                if event.key == pygame.K_LEFT:
+                    player.move(Vector2(-1, 0))
+                if event.key == pygame.K_RIGHT:
+                    player.move(Vector2(1, 0))
+                if event.key == pygame.K_UP:
+                    player.move(Vector2(0, -1))
+                if event.key == pygame.K_DOWN:
+                    player.move(Vector2(0, 1))
+                if event.key == pygame.K_1:
+                    player.move(Vector2(0, 0))
+
+        enemy1.randmove(movecounter) ####ruch bytów
+        enemy2.randmove(movecounter)
+        enemy3.randmove(movecounter)
+        stranger.randmove(movecounter)
+        movecounter += 1
+
+        screen.fill((0,0,0)) ###rysowanie
+        player.draw_player()
+        player.update()
+        enemy1.draw_enemy()
+        enemy1.update()
+        enemy2.draw_enemy()
+        enemy2.update()
+        enemy3.draw_enemy()
+        enemy3.update()
+        stranger.draw_stranger()
+        stranger.update()
+
+        #### INTERAKCJE POMIĘDZY GRACZEM A BYTAMI (testowo)####
+        if ((abs(player.pos.x - enemy1.pos.x) < cell_size) and (abs(player.pos.y - enemy1.pos.y) < cell_size)):
+            enemy1.color = (pygame.Color("blue"))
+        if ((abs(player.pos.x - enemy2.pos.x) < cell_size) and (abs(player.pos.y - enemy2.pos.y) < cell_size)):
+            enemy2.color = (pygame.Color("blue"))
+        if ((abs(player.pos.x - enemy1.pos.x) < cell_size) and (abs(player.pos.y - enemy3.pos.y) < cell_size)):
+            enemy3.color = (pygame.Color("blue"))
+        if ((abs(player.pos.x - stranger.pos.x) < cell_size) and (abs(player.pos.y - stranger.pos.y) < cell_size)):
+            player.color = (pygame.Color("yellow"))
+        #### RYSUJE SIATKĘ W TLE (testowo)####
+        for x in range((width//cell_number)*2):
+            pygame.draw.line(screen, (0,0,90), (x*cell_number/2, 0), (x*cell_number/2, height))
+        for x in range((height//cell_size)*2):
+            pygame.draw.line(screen, (0,0,90), (0, x*cell_size/2),(width, x*cell_size/2))
+
     pygame.display.update()
     pygame.display.set_caption("PIRS")
     clock.tick(60)
